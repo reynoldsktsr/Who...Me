61fac86c16b1af5d62480c0d822eccbbe32b55be
reynoldsktsr <reynoldsktsr@gmail.com>
2014-12-01 23:35:52 -0800

    Uploaded Whole Project

---
+++ www/js/app.js
@@ -0,0 +1,117 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if(window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+
+		.state('app', {
+			url: "/app",
+			abstract: true,
+			templateUrl: "templates/anonMenu.html",
+			controller: 'AppCtrl'
+		})
+
+		.state('app.search', {
+			url: "/search",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/search.html"
+				}
+			}
+		})
+
+		.state('app.browse', {
+			url: "/browse",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/browse.html"
+				}
+			}
+		})
+		.state('app.playlists', {
+			url: "/playlists",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/playlists.html",
+					controller: 'PlaylistsCtrl'
+				}
+			}
+		})
+
+		.state('app.single', {
+			url: "/playlists/:playlistId",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/playlist.html",
+					controller: 'PlaylistCtrl'
+				}
+			}
+		})
+
+		.state('app.home', {
+			url: "/home",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/home.html"
+				}
+			}
+		})
+		.state('app.start' ,{
+			url: "/start",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/start.html"
+				}
+			}
+		})
+		.state('app.register',{
+			url: "/register",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/register.html"
+				}
+			}
+		})
+
+		//BEGIN LOGGED IN
+
+		.state('app.quizHome', {
+			url: "/quizHome",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/quizHome.html"
+				}
+			}
+		})
+		.state('app.quiz1',{
+			url: "/quiz1",
+			views: {
+				'menuContent' :{
+					templateUrl: "templates/quizlib/quiz1/quiz1_1.html"
+				}
+			}
+		})
+		;
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/app/home');
+});
+
---
+++ www/js/parse.js
@@ -0,0 +1,32 @@
+Parse.initialize("jPjX42UDtpJlCpl61bVM0fZhrCo7KejSUTPZ2QrB", "hjwkwzXvZqK5XlyeCOTRD5OakMgVN4LuzuYTEuYH");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+});
+
+function registerNew(){
+var user = new Parse.User();
+var form = document.getElementById("register-form")
+var fname = form.fname.value;
+var lname = form.lname.value;
+var email = form.email.value;
+
+user.set("username", fname);
+user.set("password", lname);
+user.set("email", email);
+  
+// other fields can be set just like with Parse.Object
+user.set("phone", "650-555-0000");
+  
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+    form.submit;
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+}
